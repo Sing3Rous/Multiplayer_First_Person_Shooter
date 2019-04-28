c28903a470035efdd82a2caefdff327366fc2a39
David <singerous@gmail.com>
2019-04-29 00:42:50 +0700

    update player setup script
    * code refactor: add commentaries
    * add player ui component: crosshair
    * add control of displaying objects

--- Scripts/PlayerSetup.cs
+++ Scripts/PlayerSetup.cs
@@ -10,10 +10,21 @@ public class PlayerSetup : NetworkBehaviour
     [SerializeField]
     string remoteLayerName = "RemotePlayer";
 
+    [SerializeField]
+    string dontDrawLayerName = "DontDraw";
+    [SerializeField]
+    GameObject playerGraphics;
+
+    [SerializeField]
+    GameObject playerUIPrefab;
+    private GameObject playerUIInstance;
+
     Camera sceneCamera;
 
     void Start()
     {
+        //Disable components that should only be
+        //active on the player that we control
         if (!isLocalPlayer)
         {
             DisableComponents();
@@ -21,12 +32,33 @@ public class PlayerSetup : NetworkBehaviour
         }
         else
         {
+            //We are the local player: disable the scene camera
             sceneCamera = Camera.main;
             if (sceneCamera != null)
             {
                 sceneCamera.gameObject.SetActive(false);
             }
+
+            //Disable player graphics for local player
+            SetLayerRecursively(playerGraphics, LayerMask.NameToLayer(dontDrawLayerName));
+
+            //Create PlayerUI
+            playerUIInstance = Instantiate(playerUIPrefab);
+            playerUIInstance.name = playerUIPrefab.name;
         }
+
+        GetComponent<Player>().Setup();
+    }
+
+    void SetLayerRecursively(GameObject obj, int newLayer)
+    {
+        obj.layer = newLayer;
+
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, newLayer);
+        }
+
     }
 
     public override void OnStartClient()
@@ -53,6 +85,8 @@ public class PlayerSetup : NetworkBehaviour
 
     void OnDisable()
     {
+        Destroy(playerUIInstance);
+
         if (sceneCamera != null)
         {
             sceneCamera.gameObject.SetActive(true);
