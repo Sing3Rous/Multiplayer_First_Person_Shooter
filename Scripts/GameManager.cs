df5b7839e3f70d2d7412f311c4fc64a80db21536
David <singerous@gmail.com>
2019-04-28 14:54:23 +0700

    add GameManager.cs

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+public class GameManager : MonoBehaviour
+{
+    private const string PLAYER_ID_PREFIX = "Player ";
+    private static Dictionary<string, Player> players = new Dictionary<string, Player>();
+
+    public static void RegisterPlayer(string netID, Player player)
+    {
+        string playerID = PLAYER_ID_PREFIX + netID;
+        players.Add(playerID, player);
+        player.transform.name = playerID;
+    }
+
+    public static void UnRegisterPlayer(string playerID)
+    {
+        players.Remove(playerID);
+    }
+
+    public static Player GetPlayer(string playerID)
+    {
+        return players[playerID];
+    }
+
+    // void OnGUI()
+    // {
+    //     GUILayout.BeginArea(new Rect(200, 200, 200, 500));
+    //     GUILayout.BeginVertical();
+
+    //     foreach(string playerID in players.Keys)
+    //     {
+    //         GUILayout.Label(playerID + "  -  " + players[playerID].transform.name);
+    //     }
+
+    //     GUILayout.EndVertical();
+    //     GUILayout.EndArea();
+    // }
+}
