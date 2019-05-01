baf3e3a35294b68e8d723b18ce0fcdd512dd6b91
David <singerous@gmail.com>
2019-05-01 22:02:48 +0700

    add util script

---
+++ Scripts/Util.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Util
+{
+    public static void SetLayerRecursively (GameObject obj, int layer)
+    {
+        if (obj == null)
+        {
+            return;
+        }
+
+        obj.layer = layer;
+
+        foreach (Transform child in obj.transform)
+        {
+            if (child == null)
+            {
+                continue;
+            }
+
+            SetLayerRecursively(child.gameObject, layer);
+        }
+    }
+}
