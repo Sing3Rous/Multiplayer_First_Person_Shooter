6bc00f20eb81ca2400df959dfe28040393b3a374
David <singerous@gmail.com>
2019-05-01 22:03:16 +0700

    add player ui script

---
+++ Scripts/PlayerUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform thrusterFuelFill;
+
+    private PlayerController controller;
+
+    public void SetController(PlayerController _controller)
+    {
+        controller = _controller;
+    }
+
+    void Update()
+    {
+        SetFuelAmount(controller.getThrusterFuelAmount());
+    }
+
+    void SetFuelAmount(float amount)
+    {
+        thrusterFuelFill.localScale = new Vector3(1f, amount, 1f);
+    }
+}
