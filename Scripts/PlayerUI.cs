584873752a6800564334d0541a72670949210463
David <singerous@gmail.com>
2019-05-03 23:10:38 +0700

    add pause control

--- Scripts/PlayerUI.cs
+++ Scripts/PlayerUI.cs
@@ -5,6 +5,9 @@ public class PlayerUI : MonoBehaviour
     [SerializeField]
     RectTransform thrusterFuelFill;
 
+    [SerializeField]
+    GameObject pauseMenu;
+
     private PlayerController controller;
 
     public void SetController(PlayerController _controller)
@@ -12,9 +15,25 @@ public class PlayerUI : MonoBehaviour
         controller = _controller;
     }
 
+    void Start()
+    {
+        PauseGame.IsOn = false;
+    }
+
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+
         SetFuelAmount(controller.getThrusterFuelAmount());
+    }
+
+    void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+        PauseGame.IsOn = pauseMenu.activeSelf;
     }
 
     void SetFuelAmount(float amount)
