c9f817c4531c3ea05c62a2154c0fcd37135c4742
David <singerous@gmail.com>
2019-05-05 23:13:07 +0700

    add player stats

---
+++ Scripts/PlayerStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    public Text killCount;
+    public Text deathCount;
+
+    void Start()
+    {
+        UserAccountManager.instance.GetData(OnReceivedData);
+    }
+
+    void OnReceivedData(string data)
+    {
+        if (killCount == null || deathCount == null)
+        {
+            return;
+        }
+        
+        killCount.text = DataTranslator.DataToKills(data).ToString() + " kills.";
+        deathCount.text = DataTranslator.DataToDeaths(data).ToString() + " deaths.";
+    }
+}
