8bd5635dde966daa4a26332448caa15b2334a314
David <singerous@gmail.com>
2019-05-05 23:11:58 +0700

    add ui image rotation

---
+++ Scripts/ImageRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ImageRotation : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 200f;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0f, 0f, speed * Time.fixedDeltaTime);
+    }
+}
