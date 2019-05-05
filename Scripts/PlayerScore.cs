fd89290871dfd48d96910631b411cc12b876f623
David <singerous@gmail.com>
2019-05-05 23:14:07 +0700

    add player score counting

---
+++ Scripts/PlayerScore.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class PlayerScore : MonoBehaviour
+{  
+    int lastKills = 0;
+    int lastDeaths = 0;
+    Player player;
+    void Start()
+    {
+        player = GetComponent<Player>();
+        StartCoroutine(SyncScoreLoop());
+    }
+
+    void OnDestroy()
+    {
+        if (player != null) 
+        {
+            SyncNow();
+        }
+    }
+    IEnumerator SyncScoreLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);            
+            SyncNow();
+        }
+    }
+
+    void SyncNow()
+    {
+        UserAccountManager.instance.GetData(OnDataRecieved);
+    }
+
+    void OnDataRecieved(string data)
+    {
+        if (player.kills <= lastKills && player.deaths <= lastDeaths)
+        {
+            return;
+        }
+
+        int killsSinceLast = player.kills - lastKills;
+        int deathsSinceLast = player.deaths - lastDeaths;
+
+        int kills = DataTranslator.DataToKills(data);
+        int deaths = DataTranslator.DataToDeaths(data);
+
+        int newKills = killsSinceLast + kills;
+        int newDeaths = deathsSinceLast + deaths;
+
+        string newData = DataTranslator.ValuesToData(newKills, newDeaths);
+
+        Debug.Log("Syncing: " + newData);
+
+        lastKills = player.kills;
+        lastDeaths = player.deaths;
+
+        UserAccountManager.instance.SendData(newData);
+    }
+}
