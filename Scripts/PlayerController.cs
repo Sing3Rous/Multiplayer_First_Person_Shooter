955c5fe079e50af49dd9de7cc60c7c3089c834f3
David <singerous@gmail.com>
2019-05-05 23:14:21 +0700

    fix pause state

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -50,7 +50,22 @@ public class PlayerController : MonoBehaviour
     {
         if (PauseGame.IsOn)
         {
+            if (Cursor.lockState != CursorLockMode.None)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+
+            motor.Move(Vector3.zero);
+            motor.Rotate(Vector3.zero);
+            motor.RotateCamera(0f);
+            motor.ApplyThruster(Vector3.zero);
+
             return;
+        }
+
+        if (Cursor.lockState != CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
         }
         
         //Setting target position for spring
