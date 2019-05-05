c76d988ff6d5057d5f84bc0fc10927ff2197cc20
David <singerous@gmail.com>
2019-05-05 23:12:51 +0700

    add scoreboard

---
+++ Scripts/Scoreboard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerScoreboardItem;
+
+    [SerializeField]
+    Transform playerScoreboardList;
+
+    void OnEnable()
+    {
+        Player[] players = GameManager.GetAllPlayer();
+
+        foreach (Player player in players)
+        {
+            GameObject itemGO = (GameObject)Instantiate(playerScoreboardItem, playerScoreboardList);
+            PlayerScoreboardItem item = itemGO.GetComponent<PlayerScoreboardItem>();
+            if (item != null)
+            {
+                item.Setup(player.username, player.kills, player.deaths);
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        foreach (Transform child in playerScoreboardList)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+}
