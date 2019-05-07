7bc5e29eacc4d8fa53fdd27757b2f22087bc4d30
David <singerous@gmail.com>
2019-05-08 00:42:40 +0700

    add killfeed

---
+++ Scripts/Killfeed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killfeed : MonoBehaviour
+{
+    [SerializeField]
+    GameObject killFeddItemPrefab;
+
+    void Start()
+    {
+        GameManager.instance.onPlayerKilledCallback += OnKill;
+    }
+
+    public void OnKill(string player, string source)
+    {
+        GameObject go = (GameObject)Instantiate(killFeddItemPrefab, this.transform);
+        go.GetComponent<KillfeedItem>().Setup(player, source);
+
+        Destroy(go, 8f);
+    }
+}
